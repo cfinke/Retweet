590ba6f4096482868632babe7984316088157443
cfinke <cfinke@gmail.com>
2009-03-31 19:56:43 +0000

    Some more documentation

--- retweet.py
+++ retweet.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A bot that republishes Twitter messages sent to it."""
+
+__author__ = 'cfinke@gmail.com'
+__version__ = '1.1'
+
 import sys
 import re
 import sqlite3
@@ -5,7 +24,10 @@ import datetime
 
 import twitter as twitterpy
 
+# This is a list of accounts; username first, password second
 ACCOUNTS = [("username","password")]
+
+# The full path to the directory you want the sqlite file stored in.
 DB_DIR = "/full/path/to/dir/for/sqlite/"
 
 def retweet(initial_status_id=None):
